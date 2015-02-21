1b40389fbfe779c93e9cb28781da5972547d8eb0
OleOle7177 <ole_@bk.ru>
2015-02-21 14:45:01 +0600

    Merge conflict resolve

---
+++ app/models/event_instance.rb
@@ -0,0 +1,29 @@
+class EventInstance 
+	include ActiveModel::AttributeMethods
+
+	attr_accessor :name, :date, :event_id, :user_id
+
+
+		def self.occurrences_between(options = {}) 
+		
+		start_date = options[:start_date]
+		end_date = options[:end_date]
+		user_id = options[:current_id]
+		results = user_id ? Event.where(user_id: user_id).where("(date > ? AND date < ? AND repeat == ?) OR (date < ? and repeat != ?)", start_date, end_date, '0', end_date, '0') : Event.where("(date > ? AND date < ? AND repeat == ?) OR (date < ? and repeat != ?)", start_date, end_date, '0', end_date, '0')
+			
+		results.map { |event| 
+		event.schedule.occurrences_between(start_date, end_date).map { |date|
+			i = EventInstance.new()
+			i.name = event.name
+			i.date = date.to_date
+			i.event_id = event.id
+			i.user_id = event.user_id
+			i
+		}
+	}.flatten.sort! {|a,b| a.date <=> b.date}	
+	end
+
+
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20150223023916) do
     t.integer  "user_id"
   end
 
+ActiveRecord::Schema.define(version: 20150221082915) do
+
+
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
