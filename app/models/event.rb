ca3b713e3520d0a92a4c39ba61c738cb38c28b99
OleOle7177 <ole_@bk.ru>
2015-02-23 12:47:32 +0600

    Icecube gem installed

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,22 @@
 class Event < ActiveRecord::Base
+	include IceCube
 	belongs_to :user
 
 	validates :name, presence: true
 	validate :date, presence: true
+
+	def schedule
+		Schedule.new(self.date) do |s|
+			case self.repeat
+				when 1 
+				 	s.add_recurrence_rule IceCube::Rule.daily
+				when 7
+					s.add_recurrence_rule IceCube::Rule.weekly
+				when 30
+					s.add_recurrence_rule IceCube::Rule.monthly
+				when 365
+					s.add_recurrence_rule IceCube::Rule.yearly
+				end			
+		end
+	end
 end
